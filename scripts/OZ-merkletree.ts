705e7469f838d1334204ebdbf8ce4fdae1cd4827
Juan Xavier Valverde <juanxaviervm@hotmail.com>
2023-05-11 19:11:17 -0500

    update oz

--- scripts/OZ-merkletree.ts
+++ scripts/OZ-merkletree.ts
@@ -3,18 +3,17 @@ import fs from "fs";
 
 const data = [
   ["0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db", "10"],
-  ["0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB", "25"],
+  ["0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB", "20"],
+  ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", "30"],
+  ["0x70997970C51812dc3A010C7d01b50e0d17dc79C8", "40"],
+  ["0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC", "50"],
+  ["0x90F79bf6EB2c4f870365E785982E1f101E93b906", "60"],
+  ["0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65", "70"],
+  ["0xa0Ee7A142d267C1f36714E4a8F75612F20a79720", "80"],
 ];
 /* --------------------- CREATE TREE -------------------- */
 
-// const tree = StandardMerkleTree.of(data, ["address", "uint256"]);
-const tree = StandardMerkleTree.of(
-  [
-    ["0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db", "10"],
-    ["0x78731d3ca6b7e34ac0f824c42a7cc18a495cabab", "25"],
-  ],
-  ["address", "uint256"]
-);
+const tree = StandardMerkleTree.of(data, ["address", "uint256"]);
 
 console.log("Tree:", tree.render());
 
